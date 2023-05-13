0af3812eda1b1bccb092d0cb80aba3b1562032a8
Sait <sait108@proton.me>
2023-05-13 09:13:53 +0200

    feat: allow delete, create, read of persons

---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const app = express();
+
+// initial state of persons array
+let persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+];
+
+// helper to generate a unique id
+const generateId = () => {
+  return Math.floor(Math.random() * 10000);
+};
+
+// handle json properly
+app.use(express.json());
+
+// allow for person creation
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'content missing',
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    ...request.body,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+// root page returns Hello world
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>');
+});
+
+// info page returns info
+app.get('/info', (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date().toString()}</p>`
+  );
+});
+
+// persons route returns current state of persons
+app.get('/api/persons', (request, response) => {
+  response.json(persons);
+});
+
+// get single person
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+// delete a single person
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
