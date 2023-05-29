c79817d8b5ba31476b51141d37116c119301903e
sait39 <sait108@protonmail.com>
2023-05-29 17:47:12 +0200

    refactor: reformat and remove unnecessary local data

--- index.js
+++ index.js
@@ -1,44 +1,21 @@
 require('dotenv').config();
 const express = require('express');
-const morgan = require('morgan')
-const cors = require('cors')
+const morgan = require('morgan');
+const cors = require('cors');
+
+const PhonebookEntry = require('./models/phonebookEntry');
 
 const app = express();
 
-morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
+morgan.token('data', function (req, res) {
+  return JSON.stringify(req.body);
+});
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
-app.use(cors())
-app.use(express.static('build'))
-
-// initial state of persons array
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
-
-// helper to generate a unique id
-const generateId = () => {
-  return Math.floor(Math.random() * 10000);
-};
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :data')
+);
+app.use(cors());
+app.use(express.static('build'));
 
 // handle json properly
 app.use(express.json());
